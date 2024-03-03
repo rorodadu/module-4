5d2fa76e59d7b640a04f974d6f545b684d031fa2
rorodadu <115711182+rorodadu@users.noreply.github.com>
2024-03-03 15:55:08 +0100

    update

--- assignment 4/script.js
+++ assignment 4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
 names[0]="Yaakov";
-names[1]="mark";
-names[2]="Justin";
-names[3]="malak";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
 names[4]="paul";
-names[5]="roro";
-names[6]="lucas";
-names[7]="jean";
-names[8]="william";
-names[9]="marie";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
